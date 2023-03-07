b0eedfa2ebb297c74c0f27697d1b8cbf8b2b710b
TienTung <tientung03.nttvn@gmail.com>
2023-03-07 22:27:13 +0700

    chưa debug xong menu

--- Js/document.js
+++ Js/document.js
@@ -1,28 +1,51 @@
  // Lấy tất cả các menu items và sections
+let menu_Btn=document.querySelector('#menu-btn');
+let navbar=document.querySelector('.nav');
+menu_Btn.onclick=()=>{
+    menu_Btn.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+// const menuActives= document.querySelectorAll('.left-sibar>li');
+// const subMenuActives=document.querySelectorAll('.sub-menu');
+// for(var i=0;i<menuActives.length;i++){
+//   menuActives[i].addEventListener('click',function(){
+//     menuActives.forEach(function(menuActive){
+//       menuActive.classList.remove('active');
+//     })
+//     subMenuActives.forEach(function(subMenuActive){
+//       subMenuActive.classList.remove('active');
+//     })
+//     this.classList.add('active');
+//     subMenuActives[i].classList.add('active');
+//   })
+// }
+// làm hiệu ứng xuất hiện cho submenu
+//  menuActives[0].classList.add('active');
+//  subMenuActives[0].classList.add('active');
+//  menuActives.forEach(function(menuActive){
+//     menuActive.addEventListener('click',function(){
+//       menuActives.forEach(function(menuActive){
+//         menuActive.classList.remove('active');
+//       })
+//       subMenuActives.forEach(function(subMenuActive){
+//         subMenuActive.classList.remove('active');
+//       })
+//       // Thêm class "active" vào menu item được click và section tương ứng
+//      const target = this.getAttribute('data-target');
+//      const section = document.getElementById(target);
+//      this.classList.add('active');
+//      section.classList.add('active');
+//     })
+//  });
+ // Lặp qua các menu items và thêm event listener để khi click vào sẽ chuyển đến phần tử tương ứng
  const menuItems = document.querySelectorAll('.sub-menu li');
  const sections = document.querySelectorAll('.right-main');
- const menuActive= document.querySelectorAll('.left-sibar li');
- const subMenuActive=document.querySelectorAll('.sub-menu');
- // làm hiệu ứng xuất hiện cho submenu
- menuActive.forEach(function(item){
-    item.addEventListener('click',function(){
-      menuActive.forEach(function(item){
-        item.classList.remove('active');
-      })
-      subMenuActive.forEach(function(item){
-        item.classList.remove('sibar-li_active');
-      })
-      this.classList.add('active');
-      this.classList.add('sibar-li_active');
-    })
- });
- // Lặp qua các menu items và thêm event listener để khi click vào sẽ chuyển đến phần tử tương ứng
  menuItems.forEach(function(item) {
    item.addEventListener('click', function() {
      // Loại bỏ class "active" khỏi tất cả các menu items và sections
      menuItems.forEach(function(item) {
        item.classList.remove('active');
-     });
+     });  
      sections.forEach(function(section) {
        section.classList.remove('active');
      });
